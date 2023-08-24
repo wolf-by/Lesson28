c6b5727b0b1ae3731ab1998b7e29011b7d8d7c27
wolf-by <wolf.by@yandex.ru>
2023-08-24 14:10:04 +0300

    Add textearea to /details/

--- app.rb
+++ app.rb
@@ -74,10 +74,16 @@ end
 # вывод информации о посте
 get '/details/:post_id' do
 
+	# получаем переменную url
 	post_id = params[:post_id]
 
+	# получаем список постов 
+	# у нас будет только один пост
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	
+	# выбираем этот один пост в переменную @row
 	@row = results[0]
 
+	# возвращаем представление details.erb
 	erb :details
 end
