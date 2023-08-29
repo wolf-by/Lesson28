367745880ced861e5d856d13b78a32d120eb6646
wolf-by <wolf.by@yandex.ru>
2023-08-29 11:53:44 +0300

    Display comments for post

--- app.rb
+++ app.rb
@@ -94,7 +94,7 @@ get '/details/:post_id' do
 	@row = results[0]
 
 	# выбираем комментарии для нашего поста
-	@comments = @db.execute 'select * from Comments where post_id = ? order by id'
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 
 	# возвращаем представление details.erb
 	erb :details
