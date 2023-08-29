99d6fdec8d2d8422c5dfa25dd7c2c4708e0ff446
wolf-by <wolf.by@yandex.ru>
2023-08-29 11:28:42 +0300

    Select comments from table

--- app.rb
+++ app.rb
@@ -93,6 +93,9 @@ get '/details/:post_id' do
 	# выбираем этот один пост в переменную @row
 	@row = results[0]
 
+	# выбираем комментарии для нашего поста
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id'
+
 	# возвращаем представление details.erb
 	erb :details
 end
